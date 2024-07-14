bf566357f693e997e555bda322d4671fd078153a
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-07-14 19:57:41 +0530

    Time: 1 ms (81.31%), Space: 45.4 MB (72.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int ans[][] = new int [n][m];
+       
         HashSet<Integer> row = new HashSet<>();
         HashSet<Integer> col = new HashSet<>();
 
@@ -16,34 +16,25 @@ class Solution {
             }
         }
 
-       // System.out.println(row);
-       // System.out.println(col);
+       
         for(int i=0;i<n;i++){
             if(row.contains(i)){
-                Arrays.fill(ans[i],0);
+                Arrays.fill(matrix[i],0);
                 continue;
             }
-            for(int j=0;j<m;j++){
-                ans[i][j] = matrix[i][j];
-            }
+            
         }
 
         for(int i=0;i<m;i++){
             if(col.contains(i)){
                 for(int k=0;k<n;k++){
-                    ans[k][i] = 0;
+                    matrix[k][i] = 0;
                 }
             }
             
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                //System.out.print(ans[i][j]+" ");
-                matrix[i][j] = ans[i][j];
-            }
-            //System.out.println(" ");
-        }
+       
 
         
     }
