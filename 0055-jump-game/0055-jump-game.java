9a7c7487b8c6e144ef2aafa54d9a5130e3741d69
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-07-23 17:43:56 +0530

    Time: 324 ms (9.24%), Space: 45.5 MB (47.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,7 +1,7 @@
 class Solution {
 
-    public boolean rec(int index,int nums[],int dp[]){
-        if(index>=nums.length-1){
+    public boolean rec(int index,int nums[],int  dp[]){
+        if(index>=nums.length || index==nums.length-1){
             return true;
         }
 
@@ -12,41 +12,21 @@ class Solution {
             return false;
         }
 
-        int nindex = index+nums[index];
-        for(int i=index+1;i<=nindex;i++){
-            boolean yt = rec(i,nums,dp);
-            if(yt){
+        boolean take = false;
+        for(int i=1;i<=nums[index];i++){
+            take = rec(index+i,nums,dp);
+            if(take){
                 dp[index] = 1;
                 return true;
             }
         }
         dp[index] = 0;
         return false;
-
     }
     public boolean canJump(int[] nums) {
-        int dp[] = new int[nums.length];
+        int n = nums.length;
+        int dp[] = new int[n];
         Arrays.fill(dp,-1);
-        dp[nums.length-1] = 1;
-        for(int index=nums.length-2;index>=0;index--){
-
-            int nindex = index+nums[index];
-
-            for(int i=index+1;i<=nindex;i++){
-                int  yt = dp[i];
-                if(yt==1){
-                    dp[index] = 1;
-                    break;
-                }
-            }
-            if(dp[index]==-1){
-                dp[index] = 0;
-            }
-        } 
-
-        if(dp[0]==1){
-            return true;
-        }
-        return false;
+        return rec(0,nums,dp);
     }
 }
