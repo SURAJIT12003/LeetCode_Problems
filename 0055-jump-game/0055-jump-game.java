1125e83d300d3e1ee3747cd4a4ca8fcc28097c33
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-07-23 17:56:29 +0530

    Time: 1571 ms (5%), Space: 46 MB (7.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -26,7 +26,33 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return rec(0,nums,dp);
+       
+        dp[n-1]  = 1;
+        for(int index=n-2;index>=0;index--){
+             boolean take = false;
+            for(int i=1;i<=nums[index];i++){
+               if(index+i<n){
+
+                 if(dp[index+i]==1){
+                    take = true;
+                 }
+               }
+              
+            }
+
+            if(take){
+                dp[index] = 1;
+            }
+            else{
+                 dp[index] = 0;
+            }
+           
+           
+        }
+
+        if(dp[0]==1){
+            return true;
+        }
+        return false;
     }
 }
