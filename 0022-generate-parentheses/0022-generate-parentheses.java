0b4de07635d8b2f80935c7f1521a8a4f19794dec
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-07-25 21:45:41 +0530

    Time: 32 ms (5.35%), Space: 45.3 MB (5.45%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=s.length()-1;i>=0;i--){
+            char ch  = s.charAt(i);
+            if(ch==')'){
+                st.push(ch);
+            }
+            else{
+                if(!st.isEmpty()){
+                    char r  = st.peek();
+
+                   // System.out.println(ch+" "+r);
+                   
+                    if(ch=='(' && r!=')'){
+                        return false;
+                    }
+                   
+                    st.pop();
+                }
+                else{
+                    st.push(ch);
+                }
+            }
+              //System.out.println(st);
+
+
+        }
+
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public void rec(String s,int n,List<String> ans){
+        if(s.length()==n){
+            if(isValid(s)){
+                ans.add(s);
+            }
+            return;
+        }
+        rec(s+"(",n,ans);
+        rec(s+")",n,ans);
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        rec("",n*2,ans);
+        System.out.println(ans);
+        return ans;
+    }
+}
