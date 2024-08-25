781fb2a1ed251bae31f341b76dffb787f1574d94
Surajit Dey <128603388+SURAJIT12003@users.noreply.github.com>
2024-08-26 00:20:20 +0530

    Time: 0 ms (100%), Space: 40.5 MB (97.42%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -1,27 +1,26 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode temp = head;
         ListNode ptr = head;
-
-        while (ptr.next != null) {
-            temp = temp.next;
-
-            if (ptr.next.next == null) {
-                ptr = ptr.next;
-                continue;
+        while(ptr!=null){
+           
+            if(ptr.next==null){
+                return temp;
             }
+            temp = temp.next;
             ptr = ptr.next.next;
         }
+
         return temp;
     }
 }
